c26d697ca38b1bc7ee2c87ce2d40662ce7fc3d7c
ohtubo <brte14020@gmail.com>
2021-06-07 00:33:08 +0000

    [Add]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def new
+
+  end
+
+  def index
+    @book = Book.new
+    @books =Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # p book
+    # logger.debug("標準出力とログファイルに記録される2")
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to '/books'
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path(book.id)  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  get 'top' => 'homes#top'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show', as: 'booklist'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_booklist'
+
+  patch 'books/:id' => 'books#update', as: 'update_booklist'
+  
+  delete 'books/:id' => 'books#destroy', as: 'destroy_bookslist'
+
+
+end
