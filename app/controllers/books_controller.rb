c697bb0d553856426074a4fdadd054d2faba89f5
ohtubo <brte14020@gmail.com>
2021-06-07 06:33:48 +0000

    [Add2]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books =Book.all
+    @books =Book.all.order(id: "DESC")
   end
 
   def show
@@ -15,12 +15,10 @@ class BooksController < ApplicationController
   def create
     # １. データを新規登録するためのインスタンス作成
     book = Book.new(book_params)
-    # p book
-    # logger.debug("標準出力とログファイルに記録される2")
     # ２. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/books'
+    # ３. Show画面へリダイレクト
+    redirect_to booklist_path(book.id)
   end
 
   def edit
@@ -34,9 +32,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path(book.id)  # 投稿一覧画面へリダイレクト
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
   end
 
   private
